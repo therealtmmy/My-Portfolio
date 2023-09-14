246839d63feefc8a67107f3439b6502454f24bb9
therealtmmy <oluwatimilehinsamson@gmail.com>
2023-09-14 16:51:51 +0100

    Contact Me

--- src/Components/HEADER/Header.jsx
+++ src/Components/HEADER/Header.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "../HEADER/Header.css"
 
 const Header = () => {
+  const [scrolled, setScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      if (scrollTop > 100) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const back = {
+    background: "none"
+  }
+
+  const backk = {
+    background: "black"
+  }
+
   return (
-    <div className='Header'>
+    <>    <div className={`${'Header'}`} style= {scrolled ?  backk  : back}>
         <div>
             <a href="">OLUWATIMILEHIN</a>
         </div>
@@ -14,6 +39,8 @@ const Header = () => {
             <button>SAY HELLO</button>
         </div>
     </div>
+    </>
+
   )
 }
 
--- src/Components/REACH ME/Footer.jsx
+++ src/Components/REACH ME/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import "../REACH ME/Footer.css"
+import { FaBehanceSquare, FaGithub, FaTwitter, FaFacebook, FaLinkedin  } from "react-icons/fa";
+
 
 const Footer = () => {
 
@@ -9,15 +11,40 @@ const Footer = () => {
   return (
     <div>
       <p className='reach'>Reach Me</p>
-      <div>
-        <a href=""></a>
-        <a href=""></a>
-        <a href=""></a>
-        <a href=""></a>
-        <a href=""></a>
-        <a href=""></a>
+        <form action="https://getform.io/f/1572c1c8-e1d7-4e82-b97d-5ced3527baf1" method="post" className='Form'>
+            <label htmlFor="Name">
+              <p>Name:</p>
+            <input type="text" name='Your Name' placeholder='Your Name'/>
+            </label>
+
+            <label htmlFor="Email">
+              <p>Email:</p>
+            <input type="text" name='Your Email' placeholder='Your Email'/>
+            </label>
+
+            <label htmlFor="Message Me">
+              <p>Message Me:</p>
+            <input type="text" name='Message Me' placeholder='How can I help you' />
+             </label>
+             <br />
+        <button className='reachBtn'>Submit</button>
+      </form>
+
+      <div className='socials'>
+        <a href="https://github.com/therealtmmy/"><FaGithub style={{ color: "white", cursor: "pointer", fontSize: "25px" }}
+        />
+        </a>
+        <a href="https://twitter.com/therealtmmy"><FaTwitter style={{ color: "white", cursor: "pointer",  fontSize: "25px" }}
+        /></a>
+        <a href="https://www.linkedin.com/in/oluwatimilehin-esan-32a66a157/"><FaLinkedin style={{ color: "white", cursor: "pointer",  fontSize: "25px" }}
+        /></a>
+        <a href="https://www.behance.net/oluwatiesan"><FaBehanceSquare
+          style={{ color: "white", cursor: "pointer",  fontSize: "25px" }}
+        /></a>
+        <a href="https://web.facebook.com/timi.esan/"><FaFacebook style={{ color: "white", cursor: "pointer",  fontSize: "25px" }}/></a>
       </div>
 
+     
      
       <p className='copyright'>Oluwatimilehin Esan <span>{year}</span></p>
     </div>
