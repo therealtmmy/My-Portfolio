66ea54dd87ac15bebdf3468fe0119bc3702fc433
therealtmmy <oluwatimilehinsamson@gmail.com>
2024-03-05 13:17:25 +0100

    Updated responsiveness

--- src/Components/HEADER/Header.jsx
+++ src/Components/HEADER/Header.jsx
@@ -2,30 +2,30 @@ import React, { useEffect, useState } from "react";
 import "../HEADER/Header.css";
 
 const Header = () => {
-  const [scrolled, setScrolled] = useState(false);
+  // const [scrolled, setScrolled] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollTop = window.scrollY;
-      if (scrollTop > 100) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    };
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     const scrollTop = window.scrollY;
+  //     if (scrollTop > 100) {
+  //       setScrolled(true);
+  //     } else {
+  //       setScrolled(false);
+  //     }
+  //   };
 
-    window.addEventListener("scroll", handleScroll);
+  //   window.addEventListener("scroll", handleScroll);
 
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+  //   return () => window.removeEventListener("scroll", handleScroll);
+  // }, []);
 
-  const back = {
-    background: "none",
-  };
+  // const back = {
+  //   background: "none",
+  // };
 
-  const backk = {
-    background: "black",
-  };
+  // const backk = {
+  //   background: "black",
+  // };
 
   return (
     <>
